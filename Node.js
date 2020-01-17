d387d99f986dffd6ce30132eb8f33ec1b266fd45
itaybnv <itaybnv01@gmail.com>
2020-01-17 13:32:57 +0200

    Added distance from function to node

--- Node.js
+++ Node.js
@@ -1,14 +1,29 @@
 // x = the x location in the node grid
 // y = the y location in the node grid
-// state = 0 if its traversable, 1 if its an obstacle
+// state = 0 -> not checked, 1 -> checking, 2 -> checked and closed, 3 -> start, 4 -> target, 5 -> obstacle
 // parent = the previous node that makes the shortest known path to this node
 class Node {
 	constructor(_x, _y) {
 		this.x = _x;
-        this.y = _y;
-        this.fScore = Number.MAX_SAFE_INTEGER;
-        this.gScore = Number.MAX_SAFE_INTEGER;
+		this.y = _y;
+		this.fScore = Number.MAX_SAFE_INTEGER;
+		this.gScore = Number.MAX_SAFE_INTEGER;
 		this.state = 0;
 		this.parent = null;
-    }
+	}
+
+	distanceFrom = node => {
+		let thisCenter = {
+			x: this.x + width / (rows * 2),
+			y: this.y + height / (cols * 2)
+		};
+		let nodeCenter = {
+			x: node.x + width / (rows * 2),
+			y: node.y + height / (cols * 2)
+		};
+
+		return Math.sqrt(
+			Math.pow(this.x - node.x, 2) + Math.pow(this.y - node.y, 2)
+		);
+	};
 }
